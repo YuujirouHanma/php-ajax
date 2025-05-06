88b11773e64464d86a4c8cf008ddf471876d01bd
YuujirouHanma <150499819+YuujirouHanma@users.noreply.github.com>
2025-05-07 00:17:56 +0700

    Delete server.php

--- server.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = htmlspecialchars($_POST['username']);
-    $comment = htmlspecialchars($_POST['comment']);
-
-    echo "<p><strong>$username:</strong> $comment</p>";
-}
-?>
